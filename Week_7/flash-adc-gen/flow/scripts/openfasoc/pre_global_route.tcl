10123f541cc36a63e0177a9e5d5d7ffae053293a
Swagatika Meher <114692581+Swagatika-Meher@users.noreply.github.com>
2023-04-23 21:16:54 +0530

    Add files via upload

---
+++ Week_7/flash-adc-gen/flow/scripts/openfasoc/pre_global_route.tcl
@@ -0,0 +1,19 @@
+# Create r_VIN net
+source $::env(SCRIPTS_DIR)/openfasoc/create_routable_power_net.tcl
+create_routable_power_net "VDD" $::env(VIN_ROUTE_CONNECTION_POINTS)
+create_routable_power_net "VSS" $::env(VIN_ROUTE_CONNECTION_POINTS)
+
+
+# NDR rules
+source $::env(SCRIPTS_DIR)/openfasoc/add_ndr_rules.tcl
+
+
+# Custom connections
+source $::env(SCRIPTS_DIR)/openfasoc/create_custom_connections.tcl
+if {[info exist ::env(VDD_CUSTOM_CONNECTION)]} {
+  create_custom_connections $::env(VDD_CUSTOM_CONNECTION)
+}
+
+if {[info exist ::env(VSS_CUSTOM_CONNECTION)]} {
+  create_custom_connections $::env(VSS_CUSTOM_CONNECTION)
+}
